56072e45c38d8d2ea24b693bf171fa31f8a7c1f4
Mariam <s>
2024-04-18 23:02:56 +0200

    Add project files.

---
+++ FIHR TASK LAST/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FIHR_TASK_LAST;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+
+using System.Windows.Forms;
+
+
+namespace FIHR_TASK_LAST
+
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
